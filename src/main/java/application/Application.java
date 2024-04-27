da6e557a13a5ee48ef7ab7e9b67c684c448c071d
Arsheep Mann <asm32@sfu.ca>
2024-04-26 21:59:05 -0700

    Instance to run the application

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,15 +1,23 @@
 package application;
 
-import application.model.CourseOfferings;
-import application.model.CsvParser;
+import application.model.Course;
+import application.model.CoursePlanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main driver of SpringBoot Application
+ */
 @SpringBootApplication
 public class Application {
+    public static CoursePlanner coursePlanner = new CoursePlanner("data/course_data_2018.csv");
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
-//        CourseOfferings courseOfferings = new CourseOfferings("data/course_data_2018.csv");
-        CourseOfferings courseOfferings = new CourseOfferings("data/small_data.csv");
     }
+
+    public static CoursePlanner getCoursePlanner() {
+        return coursePlanner;
+    }
+
 }
